d4170b50766a29f93668571717ca58915893a342
Arnold Lau <arnoldlindlau@gmail.com>
2015-02-04 13:11:37 +0800

    Completed plot 3

---
+++ plot3.R
@@ -0,0 +1,15 @@
+# Subset the data by reading in specific rows (determined by opening the file beforehand in Notepad++)
+skip <- 66637
+nrows <- 2880
+
+# Read the data in
+power <- read.table("./data/household_power_consumption.txt", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", skip = skip, nrows = nrows)
+power$DateTime <- as.POSIXct(paste(power$Date, power$Time), format = "%d/%m/%Y %H:%M:%S") # merge Date and Time together and convert to class POSIXct
+
+png(file = "plot3.png") # open PNG graphics device
+with(power, plot(DateTime, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n"))
+with(power, points(DateTime, Sub_metering_1, type = "l"))
+with(power, points(DateTime, Sub_metering_2, type = "l", col = "red"))
+with(power, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.9)
+dev.off() # close PNG graphics device
