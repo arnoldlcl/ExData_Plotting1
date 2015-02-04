27ffc1ae8d8b1bb46401320413ab91c5c8dfb4e0
Arnold Lau <arnoldlindlau@gmail.com>
2015-02-04 11:01:50 +0800

    Removed additional libraries from plot 1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,3 @@
-if (!"lubridate" %in% rownames(installed.packages())) install.packages("lubridate")
-library(lubridate)
 power <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";")
-power$Date <- dmy(power$Date) # convert the Date variable to class POSIXct
-power$Time <- hms(power$Time) # convert the Time variable to class Period
+
 
