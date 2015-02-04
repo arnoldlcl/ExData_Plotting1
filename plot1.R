a6e70ed9888fb60193a023ad8ab26f4db7e06d3f
Arnold Lau <arnoldlindlau@gmail.com>
2015-02-04 12:03:28 +0800

    Completed plot 1... again

--- plot1.R
+++ plot1.R
@@ -4,9 +4,8 @@ nrows <- 2880
 
 # Read the data in
 power <- read.table("./data/household_power_consumption.txt", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", skip = skip, nrows = nrows)
-power$Date <- as.Date(power$Date, format = "%d/%m/%Y") # convert Date variable to class Date
-power$Time <- strptime(power$Time, format = "%H:%M:%S") # convert Time variable to class POSIXlt. This results in the current date being added as well but should not affect the time
+power$DateTime <- as.POSIXct(paste(power$Date, power$Time), format = "%d/%m/%Y %H:%M:%S") # merge Date and Time together and convert to class POSIXct
 
-png(file = "plot1.png")
-hist(power$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
-dev.off()
+png(file = "plot1.png") # open PNG graphics device
+hist(power$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red") # draw plot
+dev.off() # close PNG graphics device
